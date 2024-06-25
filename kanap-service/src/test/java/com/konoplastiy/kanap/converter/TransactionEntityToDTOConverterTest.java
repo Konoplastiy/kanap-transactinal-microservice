bc71115c6dee3a74af198b0a0502097b4c4219ce
Konoplastiy Andrew <konoplastiy@gmail.com>
2024-06-25 11:02:51 +0300

    Added some tests

--- kanap-service/src/test/java/com/konoplastiy/kanap/converter/TransactionEntityToDTOConverterTest.java
+++ kanap-service/src/test/java/com/konoplastiy/kanap/converter/TransactionEntityToDTOConverterTest.java
@@ -1,5 +1,8 @@
 package com.konoplastiy.kanap.converter;
 
+import com.konoplastiy.kanap.entity.Transaction;
+import com.konoplastiy.kanap.model.TransactionDTO;
+import org.jeasy.random.EasyRandom;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -7,11 +10,23 @@ import static org.testng.Assert.*;
 
 public class TransactionEntityToDTOConverterTest {
 
+    private TransactionEntityToDTOConverter converter;
+
     @BeforeMethod
     public void setUp() {
+        converter = new TransactionEntityToDTOConverter();
     }
 
     @Test
     public void testConvert() {
+        Transaction expected = new EasyRandom().nextObject(Transaction.class);
+        TransactionDTO actual = converter.convert(expected);
+
+        assertNotNull(actual);
+        assertEquals(actual.getTransactionType(), expected.getTransactionType());
+        assertEquals(actual.getAmount(), expected.getAmount());
+        assertEquals(actual.getTimestamp(), expected.getTimestamp());
+        assertEquals(actual.getCurrency(), expected.getCurrency());
+        assertEquals(actual.getStatus(), expected.getStatus());
     }
 }
--- kanap-service/src/test/java/com/konoplastiy/kanap/converter/TransactionEntityUpdaterTest.java
+++ kanap-service/src/test/java/com/konoplastiy/kanap/converter/TransactionEntityUpdaterTest.java
@@ -1,5 +1,7 @@
 package com.konoplastiy.kanap.converter;
 
+import com.konoplastiy.kanap.entity.Transaction;
+import org.jeasy.random.EasyRandom;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -7,11 +9,25 @@ import static org.testng.Assert.*;
 
 public class TransactionEntityUpdaterTest {
 
+    private TransactionEntityUpdater updater;
+
     @BeforeMethod
     public void setUp() {
+        updater = new TransactionEntityUpdater();
     }
 
     @Test
     public void testUpdateTransactionEntity() {
+        Transaction existingTransaction = new EasyRandom().nextObject(Transaction.class);
+        Transaction updatedTransaction  = new EasyRandom().nextObject(Transaction.class);
+
+        updater.updateTransactionEntity(existingTransaction, updatedTransaction);
+
+        assertNotNull(existingTransaction);
+        assertEquals(existingTransaction.getTransactionType(), updatedTransaction.getTransactionType());
+        assertEquals(existingTransaction.getAmount(), updatedTransaction.getAmount());
+        assertEquals(existingTransaction.getTimestamp(), updatedTransaction.getTimestamp());
+        assertEquals(existingTransaction.getCurrency(), updatedTransaction.getCurrency());
+        assertEquals(existingTransaction.getStatus(), updatedTransaction.getStatus());
     }
 }
